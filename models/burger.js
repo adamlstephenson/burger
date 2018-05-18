828c75cfd949ec0f9543564743b627d5eeee4f34
Adam Stephenson <astephensonx@gmail.com>
2018-05-17 20:51:48 -0400

    committing all of the progress I made on the plane

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,26 @@
 const orm = require("../config/orm.js");
 
-//
+var burger = {
+
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            console.log(res);
+
+            cb(res);
+        });
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            console.log(res);
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            console.log(res);
+            cb(res);
+        });
+    },
+}
 
 module.exports = burger;
